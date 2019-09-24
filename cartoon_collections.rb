6335382719b2f8fbbdd208d3da14a2fe36b48954
generous-vector-9581 <ojappah@gmail.com>
2019-09-24 23:05:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
    array.each_with_index { |word, index| puts "#{index+1} #{word}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+   array.collect { |word| word.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
